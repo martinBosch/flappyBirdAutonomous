cada1b39518b1453baff17999c0ae9d51627e760
mbosch <martinbosch17@gmail.com>
2022-02-13 12:47:19 -0300

    WIP

--- agent.py
+++ agent.py
@@ -1,45 +1,181 @@
 import random
 import math
+from time import sleep
+
 import numpy as np
 from pprint import pprint
 from flappybird import FlappyBird
 
+from collections import deque
+
+from keras.models import Model, Sequential, Input
+from keras.layers import Dense, Embedding, Reshape
+from keras.optimizer_v2.adam import Adam
+
+DO_NOTHING = 0
+FLAP = 1
+
+
 class Agent:
     def __init__(self):
-        self.flappybird =  FlappyBird();
-       
+        self.flappybird = FlappyBird()
+        self._state_size = 400 * 115
+        self._action_size = 2
+        self._optimizer = Adam(learning_rate=0.01)
+        self.expirience_replay = deque(maxlen=2000)
 
-    def init(self):    
+        self.alpha = 0.7
+        self.gamma = 0.6
+        self.epsilon = 0.0
+
+        self.q_network = self._build_compile_model()
+        self.target_network = self._build_compile_model()
+        self.alighn_target_model()
+
+    def _build_compile_model(self):
+        model = Sequential()
+        # model.add(Embedding(self._state_size, 10, input_length=1))
+        # model.add(Reshape((10,)))
+        # model.add(Dense(50, activation='relu'))
+        model.add(Input(shape=(1,)))
+        model.add(Dense(self._state_size, activation='relu'))
+        model.add(Dense(self._action_size, activation='linear'))
+
+        model.compile(loss='mse', optimizer=self._optimizer)
+        return model
+
+    def store(self, state, action, reward, next_state, terminated):
+        # print("STORE")
+        self.expirience_replay.append((state, action, reward, next_state, terminated))
+
+    def alighn_target_model(self):
+        self.target_network.set_weights(self.q_network.get_weights())
+
+    def retrain(self, batch_size):
+        # print("RETRAIN")
+        minibatch = random.sample(self.expirience_replay, batch_size)
+
+        for state, action, reward, next_state, terminated in minibatch:
+
+            target = self.q_network.predict(state)
+
+            if terminated:
+                target[0][action] = reward
+            else:
+                t = self.target_network.predict(next_state)
+                target[0][action] = reward + self.gamma * np.amax(t)
+
+            print("FIT")
+            self.q_network.fit(state, target, epochs=1, verbose=0)
+
+        print("FINISH RETRAIN")
+
+    def init(self):
         pass
 
     """
      * Method used to determine the next move to be performed by the agent.
      * now is moving random
      """
-
-    def act(self): 
-        self.observeworld()
-        if self.myRandom() == 2:
-            self.flappybird.holdKeyDown()        
+    def act(self, state):
+        action = self.action(state)
+        if action == FLAP:
+            # print("FLAP")
+            self.flappybird.hold_key_down()
         else:
-            self.flappybird.releaseKey()
+            # print("DO NOTHING")
+            self.flappybird.release_key()
 
-    def myRandom(self):
-        return random.randint(0, 10)
+        return action
 
-    def observeworld(self):
-        positions = self.flappybird.getWorldPositionObjets()
-        print("Upper block: ", positions[0])
-        print("Bottom block: ", positions[1])
-        print("Bird: ", positions[2])
-        print("Count: ",self.flappybird.counter)
+    def action(self, state):
+        q_value = self.q_network.predict(state)[0]
+        # print("Q Value:", q_value)
+
+        if np.random.uniform() < self.epsilon:
+            random_action = np.random.randint(0, 2)  # Random action (do nothing, flap)
+            # print("random action:", random_action)
+            return random_action
+        else:
+            # print("State:", state)
+            # print("Q value:", q_value)
+            if q_value[FLAP] > q_value[DO_NOTHING]:
+                # print("FLAP")
+                return FLAP
+            else:
+                # print("DO NOTHING")
+                return DO_NOTHING
+
+    def observe_world(self):
+        positions = self.flappybird.get_world_position_objets()
+
+        bottom_pipe = positions[0]
+        upper_pipe = positions[1]
+        bird = positions[2]
+        print("Bottom pipe: ", bottom_pipe)
+        print("Upper pipe: ", upper_pipe)
+        print("Bird: ", bird)
+        print("Count: ", self.flappybird.counter)
         print("Dead: ", self.flappybird.dead)
 
+        print("x:", bottom_pipe[0] - (bird[0] + bird[2]))
+        print("y:", bottom_pipe[1] - bird[1])
 
+        print("")
 
-    def run(self):  
-        self.flappybird.initGame()
+    def state(self):
+        positions = self.flappybird.get_world_position_objets()
+        bottom_pipe = positions[0]
+        bird = positions[2]
+
+        x = bottom_pipe[0] - (bird[0] + bird[2])
+        y = bottom_pipe[1] - bird[1]
+
+        # if x > 200:
+        #    x = 200
+
+        if y > 113:
+            y = 113
+
+        if y < 10:
+            y = 0
+
+        return np.array([x + 200, y])
+
+    def reward(self):
+        if not self.flappybird.dead:
+            # print("reward: 1")
+            return 1
+        else:
+            # print("reward: -1000")
+            return -1000
+
+    def run(self):
+        self.flappybird.init_game()
+        batch_size = 1
+
         while True:
-            self.flappybird.eachCicle()            
-            self.act()
+            # sleep(0.1)
+            # self.observe_world()
+            state = self.state()
+            # print("state:", state)
+            action = self.act(state)
+            # print("action:", action)
 
+            self.flappybird.each_cycle()
+
+            new_state = self.state()
+            immediate_reward = self.reward()
+            self.store(state, action, immediate_reward, new_state, self.flappybird.dead)
+            if self.flappybird.dead:
+                # print("Action:", action)
+                self.alighn_target_model()
+            else:
+                if len(self.expirience_replay) > batch_size:
+                    self.retrain(batch_size)
+
+            if self.epsilon > 0.01:
+                self.epsilon -= 0.01
+
+            if self.flappybird.dead:
+                self.flappybird.restart_game()
--- flappybird.py
+++ flappybird.py
@@ -28,40 +28,39 @@ class FlappyBird:
         self.counter = 0
         self.offset = random.randint(-110, 110)
         self.isKeyDown = False
-        
 
-    def updateWalls(self):
+    def update_walls(self):
         self.wallx -= 2
         if self.wallx < -80:
             self.wallx = 400
             self.counter += 1
             self.offset = random.randint(-110, 110)
 
-    def calculateWorldPositionObjets(self):
-        self.worldPositions =  np.array([
-                            [self.wallx,
-                             360 + self.gap - self.offset + 10,
-                             self.wallUp.get_width() - 10,
-                             self.wallUp.get_height()
-                             ],
-                             [self.wallx,
-                               0 - self.gap - self.offset - 10,
-                               self.wallDown.get_width() - 10,
-                               self.wallDown.get_height()
-                              ],
-                              [
-                                  self.bird[0],
-                                  self.bird[1],
-                                  self.bird[2],
-                                  self.bird[3]
-                              ]
-                        ])
-        return self.worldPositions
+    def calculate_world_position_objets(self):
+        self.world_positions = np.array([
+            [self.wallx,
+             360 + self.gap - self.offset + 10,
+             self.wallUp.get_width() - 10,
+             self.wallUp.get_height()
+             ],
+            [self.wallx,
+             0 - self.gap - self.offset - 10,
+             self.wallDown.get_width() - 10,
+             self.wallDown.get_height()
+             ],
+            [
+                self.bird[0],
+                self.bird[1],
+                self.bird[2],
+                self.bird[3]
+            ]
+        ])
+        return self.world_positions
 
-    def getWorldPositionObjets(self):
-         return self.worldPositions
+    def get_world_position_objets(self):
+        return self.world_positions
 
-    def birdUpdate(self):
+    def bird_update(self):
         if self.jump:
             self.jumpSpeed -= 1
             self.birdY -= self.jumpSpeed
@@ -70,56 +69,53 @@ class FlappyBird:
             self.birdY += self.gravity
             self.gravity += 0.2
         self.bird[1] = self.birdY
-        positions = self.calculateWorldPositionObjets()
-        upRect = pygame.Rect(positions[0][0],positions[0][1],positions[0][2],positions[0][3])
-        downRect = pygame.Rect(positions[1][0],positions[1][1],positions[1][2],positions[1][3])
+        positions = self.calculate_world_position_objets()
+        upRect = pygame.Rect(positions[0][0], positions[0][1], positions[0][2], positions[0][3])
+        downRect = pygame.Rect(positions[1][0], positions[1][1], positions[1][2], positions[1][3])
+        # toco el tubo de arriba?
         if upRect.colliderect(self.bird):
             self.dead = True
+        # toco el tubo de abajo?
         if downRect.colliderect(self.bird):
             self.dead = True
+        # toco el techo o toco el fondo?
         if not 0 < self.bird[1] < 720:
-            self.bird[1] = 50
-            self.birdY = 50
-            self.dead = False
-            self.counter = 0
-            self.wallx = 400
-            self.offset = random.randint(-110, 110)
-            self.gravity = 5
+            self.dead = True
 
-    def holdKeyDown(self):
+    def hold_key_down(self):
         self.isKeyDown = True
 
-    def releaseKey(self):
+    def release_key(self):
         self.isKeyDown = False
 
-    def doAction(self):
+    def do_action(self):
         self.jump = 17
         self.gravity = 0
         self.jumpSpeed = 10
 
-    def eachCicle(self):
-        clock = self.clock        
+    def each_cycle(self):
+        clock = self.clock
         font = self.font
         clock.tick(60)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
             if (event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN) and not self.dead:
-                self.doAction()
-        
-        if(self.isKeyDown):
-            self.doAction()
+                self.do_action()
+
+        if self.isKeyDown:
+            self.do_action()
 
         self.screen.fill((255, 255, 255))
         self.screen.blit(self.background, (0, 0))
         self.screen.blit(self.wallUp,
-                            (self.wallx, 360 + self.gap - self.offset))
+                         (self.wallx, 360 + self.gap - self.offset))
         self.screen.blit(self.wallDown,
-                            (self.wallx, 0 - self.gap - self.offset))
+                         (self.wallx, 0 - self.gap - self.offset))
         self.screen.blit(font.render(str(self.counter),
-                                        -1,
-                                        (255, 255, 255)),
-                            (200, 50))
+                                     -1,
+                                     (255, 255, 255)),
+                         (200, 50))
         if self.dead:
             self.sprite = 2
         elif self.jump:
@@ -127,22 +123,32 @@ class FlappyBird:
         self.screen.blit(self.birdSprites[self.sprite], (70, self.birdY))
         if not self.dead:
             self.sprite = 0
-        self.updateWalls()        
-        self.birdUpdate()
+        self.update_walls()
+        self.bird_update()
         pygame.display.update()
-        self.clock = clock        
+        self.clock = clock
         self.font = font
 
-    def initGame(self):
+    def init_game(self):
         self.clock = pygame.time.Clock()
         pygame.font.init()
         self.font = pygame.font.SysFont("Arial", 50)
-    
-    def run(self):        
-        self.initGame()
+        self.calculate_world_position_objets()
+
+    def restart_game(self):
+        self.bird[1] = 100
+        self.birdY = 350
+        self.dead = False
+        self.counter = 0
+        self.wallx = 400
+        self.offset = random.randint(-110, 110)
+        self.gravity = 5
+
+    def run(self):
+        self.init_game()
         while True:
-            self.eachCicle()
-           
+            self.each_cycle()
+
 
 if __name__ == "__main__":
     FlappyBird().run()
